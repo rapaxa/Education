a06b3c46ca17f7d2b23ab12a8198945110c5e143
Volodchenko Pavel <rapaxa123@gmail.com>
2021-12-13 14:27:54 +0200

    version 3

--- Guess_Number/script.js
+++ Guess_Number/script.js
@@ -1,45 +1,45 @@
+let rN, count
 let text = document.getElementById('text')
 let save = document.getElementById('save')
-let exit = document.getElementById('exit')
+let exit = document.querySelector('#exit')
+let message = document.querySelector('.message')
+let diapasonBlock = document.querySelector('#block_diapason')
+let blockGenerate = document.querySelector('.block_generate')
+let getBtn = document.getElementById('generate')
+
 exit.addEventListener('click', gameOver)
 save.addEventListener('click', saveBtn)
 save.addEventListener('click', getCount)
-let rN, count
-let getBtn = document.getElementById('generate')
+save.addEventListener('click', settings)
 getBtn.addEventListener('click', getGameNumber)
-
+blockGenerate.style.display = "none"
 function getCount() {
     let number = document.getElementById('number').value
     if (number >= 1 && number <= 15) {
+
         return count = number
     } else {
         alert('Введите другое значение попыток от 1 - 15')
+        return false
     }
-}
-
-function gameOver() {
-    location.reload()
 }
 
 function getGameNumber() {
 
     let getGNumber = document.getElementById('g').value
+    let score = 1;
+    score++
     if (count - 1 >= 0) {
-
         if (rN < getGNumber) {
             text.innerHTML = `Перебор, осталосб : ${count} попыток`
-
         } else if (rN > getGNumber) {
             text.innerHTML = `Недолет, осталосб : ${count} попыток`
 
         } else if (rN == getGNumber) {
-
-            text.innerHTML = "Угадал"
-            setTimeout(gameOver, 5000)
-
+            getBtn.disabled = true
+            text.innerHTML = `Угадал за ${score} попыток`
         }
         count--
-
     } else {
         alert('Игра закончена ')
         setTimeout(gameOver, 1000)
@@ -51,11 +51,12 @@ function saveBtn() {
     let getInputEnd = document.getElementById('number_finish').value
     let number = getRandomIntInclusive(getInputStart, getInputEnd)
     let count = document.getElementById('number').value
+    message.innerHTML = `Привет я загадал число от ${getInputStart}  до ${getInputEnd} попробуй отгадать за ${count} попыток`
     if (getInputStart < 1 || getInputStart > 200 || getInputEnd > 200 || getInputEnd < 1) {
         alert('Введен неправельный диапазон .Введите от 1 - 200')
-
+        return false
     } else {
-        save.disabled
+
         return rN = number;
     }
 
@@ -65,4 +66,14 @@ let getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+function gameOver() {
+    location.reload()
+}
+function settings(){
+    if((saveBtn() && getCount()) !== false){
+        diapasonBlock.style.display = 'none'
+        blockGenerate.style.display = ''
+    }
 }

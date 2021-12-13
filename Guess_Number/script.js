0776920518e3986c3f295e78acf4a4002a0ef16c
Volodchenko Pavel <rapaxa123@gmail.com>
2021-12-13 04:09:58 +0200

    version 3

--- Guess_Number/script.js
+++ Guess_Number/script.js
@@ -1,22 +1,31 @@
 let text = document.getElementById('text')
 let save = document.getElementById('save')
+let exit = document.getElementById('exit')
+exit.addEventListener('click', gameOver)
 save.addEventListener('click', saveBtn)
 save.addEventListener('click', getCount)
-let rN,count
+let rN, count
 let getBtn = document.getElementById('generate')
 getBtn.addEventListener('click', getGameNumber)
-function getCount (){
+
+function getCount() {
     let number = document.getElementById('number').value
-    if(number>1 && number<15){
+    if (number >= 1 && number <= 15) {
         return count = number
-    }else{
-        alert('Введите другое значение')
+    } else {
+        alert('Введите другое значение попыток от 1 - 15')
     }
 }
+
+function gameOver() {
+    location.reload()
+}
+
 function getGameNumber() {
 
     let getGNumber = document.getElementById('g').value
-    if (count >= 0) {
+    if (count - 1 >= 0) {
+
         if (rN < getGNumber) {
             text.innerHTML = `Перебор, осталосб : ${count} попыток`
 
@@ -24,14 +33,16 @@ function getGameNumber() {
             text.innerHTML = `Недолет, осталосб : ${count} попыток`
 
         } else if (rN == getGNumber) {
+
             text.innerHTML = "Угадал"
+            setTimeout(gameOver, 5000)
 
-        }
-
-        if(count <1){
-            alert("Игра закончена")
         }
         count--
+
+    } else {
+        alert('Игра закончена ')
+        setTimeout(gameOver, 1000)
     }
 }
 
@@ -40,8 +51,8 @@ function saveBtn() {
     let getInputEnd = document.getElementById('number_finish').value
     let number = getRandomIntInclusive(getInputStart, getInputEnd)
     let count = document.getElementById('number').value
-    if (getInputStart < 1 || getInputEnd > 200) {
-        alert('Ввод неправельный')
+    if (getInputStart < 1 || getInputStart > 200 || getInputEnd > 200 || getInputEnd < 1) {
+        alert('Введен неправельный диапазон .Введите от 1 - 200')
 
     } else {
         save.disabled
